867b4b0283040441ea80686833d5206b7684db2b
Zeptosec <armandasmatkevicius@gmail.com>
2023-02-06 14:38:30 +0200

    Fixed minor folder issue

--- src/components/Dirtem.tsx
+++ src/components/Dirtem.tsx
@@ -10,10 +10,10 @@ interface Props {
 }
 export default function DirItem({ dir, setDirHistory, selected, setSelected, MoveSelected }: Props) {
     const refSel = useRef<any>(null);
-    const refCard = useRef<any>(null);
+    const refName = useRef<any>(null);
 
     function clicked(w: any) {
-        if (refSel.current && refSel.current.contains(w.target) || refCard.current === w.target) {
+        if (refName.current && !refName.current.contains(w.target)) {
             const rez = selected.findIndex(el => el.created_at === dir.created_at);
             if (rez === -1) setSelected(el => [...el, dir]);
             else setSelected(el => [...el.slice(0, rez), ...el.slice(rez + 1)]);
@@ -21,16 +21,16 @@ export default function DirItem({ dir, setDirHistory, selected, setSelected, Mov
     }
 
     return (
-        <div ref={refCard} onClick={w => clicked(w)} className="flex justify-between card group">
+        <div onClick={w => clicked(w)} className="flex justify-between card group">
             <div className={`flex transition-all duration-200 items-center ${selected.length > 0 ? `gap-2` : ``}`}>
-                <div ref={refSel} className={`border-blue-900 rounded-full cursor-pointer flex justify-center items-center transition-all duration-200 ${selected.length > 0 ? `w-3.5 h-3.5 border` : `w-0 h-0`}`}>
+                <div className={`border-blue-900 rounded-full cursor-pointer flex justify-center items-center transition-all duration-200 ${selected.length > 0 ? `w-3.5 h-3.5 border` : `w-0 h-0`}`}>
                     <div className={`rounded-full w-2 h-2 bg-blue-700 transition-opacity duration-200 ${selected.findIndex(w => w.created_at === dir.created_at) !== -1 ? "opacity-100" : "opacity-0 group-hover:opacity-40"}`}></div>
                 </div>
                 <div className="flex gap-2 items-center justify-center">
                     <div className="w-5 h-4 m-auto sm:block hidden">
                         <i className="gg-folder m-auto text-blue-900 group-hover:text-blue-700 transition-colors duration-200"></i>
                     </div>
-                    <p onClick={w => setDirHistory(prev => [...prev, dir.id])} className="cursor-pointer">{dir.name}</p>
+                    <p ref={refName} onClick={w => setDirHistory(prev => [...prev, dir.id])} className="cursor-pointer">{dir.name}</p>
                 </div>
             </div>
             {selected.length > 0 ? <div className={`cursor flex items-center text-blue-900 hover:text-blue-700 transition-colors duration-200`}>
