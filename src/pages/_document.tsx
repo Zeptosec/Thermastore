defe2515208266c4ea41236a60da90e50a0f2181
Zeptosec <armandasmatkevicius@gmail.com>
2023-02-26 11:44:49 +0200

    Icon links changed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,23 +4,23 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <link href='https://css.gg/link.css' rel='stylesheet'/>
-        <link href='https://css.gg/arrow-left.css' rel='stylesheet'></link>
-        <link href='https://css.gg/add-r.css' rel='stylesheet'></link>
-        <link href='https://css.gg/folder-add.css' rel='stylesheet'></link>
-        <link href='https://css.gg/arrow-down-r.css' rel='stylesheet'></link>
-        <link href='https://css.gg/close-r.css' rel='stylesheet'></link>
-        <link href='https://css.gg/arrow-right.css' rel='stylesheet'/>
-        <link href='https://css.gg/remove-r.css' rel='stylesheet'/>
-        <link href='https://css.gg/file.css' rel='stylesheet'></link>
-        <link href='https://css.gg/music-note.css' rel='stylesheet'/>
-        <link href='https://css.gg/file-document.css' rel='stylesheet'></link>
-        <link href='https://css.gg/film.css' rel='stylesheet'></link>
-        <link href='https://css.gg/folder.css' rel='stylesheet'></link>
-        <link href='https://css.gg/rename.css' rel='stylesheet'/>
-        <link href='https://css.gg/play-stop-r.css' rel='stylesheet'></link>
-        <link href='https://css.gg/play-button-r.css' rel='stylesheet'></link>
-        <link href='https://css.gg/globe-alt.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/link.css' rel='stylesheet'/>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/arrow-left.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/add-r.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/folder-add.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/arrow-down-r.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/close-r.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/arrow-right.css' rel='stylesheet'/>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/remove-r.css' rel='stylesheet'/>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/file.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/music-note.css' rel='stylesheet'/>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/file-document.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/film.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/folder.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/rename.css' rel='stylesheet'/>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/play-stop-r.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/play-button-r.css' rel='stylesheet'></link>
+        <link href='https://unpkg.com/css.gg@2.0.0/icons/css/globe-alt.css' rel='stylesheet'></link>
       </Head>
       <body>
         <Main />
