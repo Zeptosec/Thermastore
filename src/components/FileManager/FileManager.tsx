7006b27924d27d30bf178370f04ac58314a64d21
Zeptosec <armandasmatkevicius@gmail.com>
2023-06-03 21:44:00 +0300

    Fixed window animations

--- src/components/FileManager/FileManager.tsx
+++ src/components/FileManager/FileManager.tsx
@@ -17,7 +17,7 @@ export default function FileManager({fm, user}:Props) {
                 <i className="gg-arrows-exchange"></i>
             </button>
             <FileManagerWindow fileManager={fm?.state} user={user !== null} dispatch={fm?.dispatch}
-                className={`sm:absolute fixed border-cyan-500 grid gap-2 overflow-hidden bg-file/80 sm:bg-blue-700/90 text-white sm:rounded-lg rounded-t-lg transition-all duration-300 sm:max-w-[400px] ${fm?.state.showMenu ? 'sm:max-h-[400px] w-screen p-2 h-[calc(100vh-72px)] sm:h-auto sm:top-[50px] top-[72px] right-0 sm:shadow-infopanel sm:border-2 border' : 'max-h-0 w-0 top-2 shadow-none right-4'}`} />
+                className={`sm:absolute fixed border-cyan-500 grid gap-2 overflow-hidden bg-file/80 sm:bg-blue-700/90 text-white sm:rounded-lg rounded-t-lg transition-all duration-300 sm:max-w-[400px] ${fm?.state.showMenu ? 'sm:max-h-[400px] w-screen p-2 h-[calc(100vh-72px)] sm:h-auto sm:top-[50px] top-[72px] right-0 sm:shadow-infopanel sm:border-2 border' : 'sm:max-h-0 max-sm:h-0 w-0 top-2 shadow-none right-4'}`} />
         </>
     )
 }
