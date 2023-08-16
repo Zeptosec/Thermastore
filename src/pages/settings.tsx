c262a4104eff345e396c63e8e61e6592398405e7
Zeptosec <armandasmatkevicius@gmail.com>
2023-08-16 19:28:33 +0300

    Fixed styling

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -12,7 +12,7 @@ export default function settingsPage() {
                 <title>Settings</title>
                 <meta key="desc" property='og:description' content='Change your theme, add streamer services and add your own custom hook.' />
             </Head>
-            <div className="h-100vh py-[100px] px-4 text-quaternary">
+            <div className="min-h-screen py-[100px] px-4 text-quaternary">
                 <p className="text-center text-4xl pb-8">Settings</p>
                 <div className="grid gap-2 text-center pb-8 max-w-[1000px] m-auto">
                     <HookManager />
