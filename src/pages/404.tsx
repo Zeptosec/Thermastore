59e84e1e8f860a8e7b109397eafaa0bf9514d57d
Zeptosec <armandasmatkevicius@gmail.com>
2023-08-16 19:27:58 +0300

    Removed unnecessary lines

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
     return (
         <div>
             {/* <BubbleBackground /> */}
-            <div className="grid items-center h-100vh max-w-[800px] m-auto px-4 gap-4 py-[72px]">
+            <div className="grid items-center min-h-screen max-w-[800px] m-auto px-4 gap-4 py-[72px]">
                 <div className="text-center grid gap-8">
                     <p className="text-8xl">Ooops...</p>
                     <p className="text-3xl">It seems that you're lost</p>
